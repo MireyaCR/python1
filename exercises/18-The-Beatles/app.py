12445047474d71ee78e73f86c65183ff33966d60
MireyaCR <mireya.mcr@gmail.com>
2022-11-15 20:43:56 +0000

    python1

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,1 +1,22 @@
 # Your code here!!
+
+
+def sing() :
+
+    lib='let it be'
+    frase1= 'whisper words of wisdom'
+    frase2= 'there will be an answer'
+    cancion =''
+
+    for i in range(1,9):
+        cancion+=lib+', ' 
+        if (i%4 == 0) :
+            if (i/4==1):
+                cancion += frase1+', '
+                cancion += lib +', '     
+            else:
+                cancion += frase2+', ' 
+                cancion += lib   
+    return cancion
+
+print(sing())
